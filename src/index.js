42c0fc851f1caf20d0c2719341478e87e65d1882
dexterous17 <dexterousprajapati@gmail.com>
2021-08-19 17:24:47 -0400

    Pointer has been added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
-import Reducer from './redux store/Store';
-import {Provider} from 'react-redux'
-
-const store = createStore(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <Provider store={store}>
+  
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
+  ,
   document.getElementById('root')
 );
